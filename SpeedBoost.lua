b78478bc8b17c990f16e08691ed5f4f3485c6284
reaper-oc <cursefab@gmail.com>
2026-06-29 15:55:25 +0100

    add code
    Removed explicit language from print statement.

---
+++ SpeedBoost.lua
@@ -0,0 +1,6 @@
+local player = game.Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+local humanoid = character:WaitForChild("Humanoid")
+
+humanoid.WalkSpeed = 250
+print("Speed boosted to " .. humanoid.WalkSpeed ..)
